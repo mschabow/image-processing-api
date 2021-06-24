c4764edefbcd8b62cd7c1289689ffae85710c828
Matt Schabowsky <mschabowsky@gmail.com>
2021-06-23 21:03:25 -0600

    Inital Project Setup

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const myFunc = (num: number): number => {
+  return num * num;
+};
+export default myFunc;
---
+++ src/tests/index.spec.ts
@@ -0,0 +1,4 @@
+import myFunc from '../index';
+it('expect myFunc(5) to equal 25', () => {
+  expect(myFunc(5)).toEqual(25);
+});
